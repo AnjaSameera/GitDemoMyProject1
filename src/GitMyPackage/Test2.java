c9fc7aa1dce449b4cf91d10dad16b4bd60bd0503
LCM <LCM@10.0.0.21>
2021-10-27 20:44:50 +1100

    Comiting Test2 to Branch1

---
+++ src/GitMyPackage/Test2.java
@@ -0,0 +1,16 @@
+package GitMyPackage;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String hungry ="Yes";
+		
+		if(hungry == "Yes")
+			System.out.println("Eat Somthing ....");
+		else {
+			System.out.println("Do Work....");
+			 }
+
+	}
+
+}
